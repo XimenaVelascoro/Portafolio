a71fc324187c1c1b8b8e5d450327e44c052e3afd
Laura Ximena Velasco Rocha <12ximenavelasco@gmail.com>
2023-02-16 16:50:55 -0500

    more files

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,25 +1,27 @@
-import React from 'react';
-import './header.css';
-import ME from '../../assets/me.jpeg';
-import CTA from './CTA';
-import HeaderSocials from './HeaderSocials';
-
-
+import React from "react";
+import "./header.css";
+import ME from "../../assets/me.jpeg";
+import CTA from "./CTA";
+import HeaderSocials from "./HeaderSocials";
 
 const Header = () => {
   return (
-    <div className='header'>
-    <h2 className='text-light'>Hi I'm  </h2>
-    <h1>Laura Ximena Velasco Rocha</h1>
-    <h2 className='text-light'>Fullstack Developer</h2>
-     <CTA/>
-     <HeaderSocials/>
-      <div className="me">
-        <img src={ME}alt="me" className='foto1' />
+    <header>
+      <div className="header">
+        <h2 className="text-light">Hi I'm </h2>
+        <h1>Laura Ximena Velasco Rocha</h1>
+        <h2 className="text-light">Fullstack Developer</h2>
+        <CTA />
+        <HeaderSocials />
+        <div className="me">
+          <img src={ME} alt="me" className="foto1" />
+        </div>
+        <a href="#contact" className="scroll__down">
+          Scoll Down{" "}
+        </a>
       </div>
-      <a href='#contact' className='scroll__down'>Scoll Down </a>
-  </div>
-  )
-}
+    </header>
+  );
+};
 
-export default Header
+export default Header;
