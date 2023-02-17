60a950a580170aaaf20bb45b62823bb30da661db
Laura Ximena Velasco Rocha <12ximenavelasco@gmail.com>
2023-02-17 09:24:34 -0500

    add a new components

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import "./nav.css";
+import {AiOutlineHome} from "react-icons/ai";
+import {SlUserFemale} from "react-icons/sl";
+import {BiBookAlt} from "react-icons/bi";
+import {RiServiceLine} from "react-icons/ri"
+import {BiMessageAltDetail} from "react-icons/bi";
+
+
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href={"#home"}><AiOutlineHome/></a>
+      <a href={"#about"}><SlUserFemale/></a>
+      <a href={"#experience"}><BiBookAlt/></a>
+      <a href={"#services"}><RiServiceLine/></a>
+      <a href={"#contact"}><BiMessageAltDetail/></a>
+    </nav>
   )
 }
-
+/*el atributo href debe tener un valor de cadena de texto en lugar de un objeto */
 export default Nav
