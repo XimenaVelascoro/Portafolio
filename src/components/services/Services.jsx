4e9aaf6359af9f3324905ffa8e02e1a6ed5b716c
Laura Ximena Velasco Rocha <12ximenavelasco@gmail.com>
2023-03-01 00:17:27 -0500

    new changes in the services file

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './services.css'
 import {GiCheckMark} from 'react-icons/gi'
 
 const Services = () => {
@@ -34,7 +35,7 @@ const Services = () => {
             </li>
             <li>
               <GiCheckMark className='service__list-icon'/>
-              <p>Simplificar la UI  permite mejorar la experiencia del este, al reducir la complejidad y el ruido visual.
+              <p>Simplificar la UI  permite mejorar la experiencia de este, al reducir la complejidad y el ruido visual
               </p>
             </li>
             <li>
@@ -47,37 +48,33 @@ const Services = () => {
         {/*END OF UX/UI EXPERIENCE */}
         <article className="service">
           <div className="service__head">
-            <h3>Web Development</h3>
+            <h3>Agile Methodologies</h3>
           </div>
           <ul className='service__list'>
             <li>
               <GiCheckMark className='service__list-icon'/>
-              <p>Conocimiento del usuario y sus necesidades, para diseñar experiencias mas efectivas y satisfactorias
+              <p>Valoracion y adaptación  acorde a las necesidades del cliente por medio de iteraciones o sprints pequeños y entregas incrementales
               </p>
             </li>
             <li>
               <GiCheckMark className='service__list-icon'/>
-              <p>Diseñar para la usabilidad, para facilitarle procesos al usuario
+              <p>Comunicación constante, retroalimentación y participación activa de todos los miembros del equipo, para lograr el objetivo que se tiene en común
+              </p>
+            </li>
+            
+            <li>
+              <GiCheckMark className='service__list-icon'/>
+              <p>Priorización de tareas; los requisitos más importantes se manejan primero
               </p>
             </li>
             <li>
               <GiCheckMark className='service__list-icon'/>
-              <p>Creación de una jerarquia visual destacando los elementos mas importantes, para asi identificarlos de manera rápida
+              <p> Hacer uso de la metodología Scrum para tener un enfoque iterativo e incremental en la gestión de proyectos
               </p>
             </li>
             <li>
               <GiCheckMark className='service__list-icon'/>
-              <p>Diseñar de manera consistente crea una experiencia coherente para el usuario en todo el producto o servicio
-              </p>
-            </li>
-            <li>
-              <GiCheckMark className='service__list-icon'/>
-              <p>Simplificar la UI  permite mejorar la experiencia del este, al reducir la complejidad y el ruido visual.
-              </p>
-            </li>
-            <li>
-              <GiCheckMark className='service__list-icon'/>
-              <p>Retroalimentación del usuario para identificar oportunidades de mejorar la calidad en cuanto la UX
+              <p>Hacer pruebas automatizadas las cuales nos sirven para que el código nuevo no afecte negativamente el código existente y  aseguran que el código nuevo funcione según lo esperado
               </p>
             </li>
           </ul>
@@ -85,37 +82,33 @@ const Services = () => {
         {/*END OF WEB DEBELOPMENT */}
         <article className="service">
           <div className="service__head">
-            <h3>UX/UI Design</h3>
+            <h3>Backlog</h3>
           </div>
           <ul className='service__list'>
+            
             <li>
               <GiCheckMark className='service__list-icon'/>
-              <p>Conocimiento del usuario y sus necesidades, para diseñar experiencias mas efectivas y satisfactorias
+              <p>Priorizar las tareas según su importancia y urgencia, permitiendo al equipo enfocarse en las funcionalidades más importantes en primer lugar
               </p>
             </li>
             <li>
               <GiCheckMark className='service__list-icon'/>
-              <p>Diseñar para la usabilidad, para facilitarle procesos al usuario
+              <p>Descripción detallada de tareas, donde se explique qué se espera de esta y cómo se debe llevar a cabo
               </p>
             </li>
             <li>
               <GiCheckMark className='service__list-icon'/>
-              <p>Creación de una jerarquia visual destacando los elementos mas importantes, para asi identificarlos de manera rápida
+              <p>Estimación de esfuerzo ya que cada tarea se debe estimar el esfuerzo necesario para completarla  y asi planificar tiempos de forma mas acertada
               </p>
             </li>
             <li>
               <GiCheckMark className='service__list-icon'/>
-              <p>Diseñar de manera consistente crea una experiencia coherente para el usuario en todo el producto o servicio
+              <p> Asignación de tareas según la capacidad y habilidades de cada integrante del grupo para llevar a cabo las tareas asignadas
               </p>
             </li>
             <li>
               <GiCheckMark className='service__list-icon'/>
-              <p>Simplificar la UI  permite mejorar la experiencia del este, al reducir la complejidad y el ruido visual.
-              </p>
-            </li>
-            <li>
-              <GiCheckMark className='service__list-icon'/>
-              <p>Retroalimentación del usuario para identificar oportunidades de mejorar la calidad en cuanto la UX
+              <p>Actualización constante del backlog según se van completando las tareas e ir actualizando las nuevas necesidades del cliente o del proyecto
               </p>
             </li>
           </ul>
